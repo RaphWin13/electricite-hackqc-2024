faf8b42c09d02f7258eaa832292392c73ce7a275
Sabrina Cazzolato <sabrina.cazzolato.1@ulaval.ca>
2024-03-10 23:48:27 -0400

    feat: create Question page and add toggle between home and question

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,30 @@
 <template>
-  <EquivalenceAnimation />
+  <v-app>
+    <v-btn class="mb-4 mx-auto" width="200" @click="togglePages">{{showQuestion? "Hide question" : "Show questions"}}</v-btn>
+    <QuestionPage v-if="showQuestion" />
+    <HomePage v-else />
+  </v-app>
 </template>
 
 <script>
-import EquivalenceAnimation from './components/equivalence-animations/EquivalenceAnimation.vue';
+import HomePage from './HomePage.vue';
+import QuestionPage from './QuestionPage.vue';
 
 export default {
   name: 'App',
   components: {
-    EquivalenceAnimation
+    HomePage,
+    QuestionPage
+  },
+  data() {
+    return {
+      showQuestion: false
+    };
+  },
+  methods: {
+    togglePages() {
+      this.showQuestion = !this.showQuestion;
+    }
   }
 }
 </script>
---
+++ src/HomePage.vue
@@ -0,0 +1,14 @@
+<template>
+  <EquivalenceAnimation />
+</template>
+
+<script>
+import EquivalenceAnimation from './components/equivalence-animations/EquivalenceAnimation.vue';
+
+export default {
+  name: 'HomePage',
+  components: {
+    EquivalenceAnimation,
+  }
+}
+</script>
