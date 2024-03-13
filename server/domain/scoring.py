554312ebf8f7030197969145c6787852412b777b
MirageMika <83600543+MirageMika@users.noreply.github.com>
2024-03-13 14:02:14 -0400

    score adapter + json function

--- server/domain/scoring.py
+++ server/domain/scoring.py
@@ -1,51 +1,89 @@
 import pandas as pd
 import numpy as np
 from datetime import datetime
+import pytz
 
+from datetime import datetime
 
-#Get actual consumption in KWh
-def getActualConso()-> float:
-  consoData = pd.read_json("..\infra\data\production.json") # A récupérer sur ce site : https://www.hydroquebec.com/data/documents-donnees/donnees-ouvertes/json/production.json
-  lastAvailableData = consoData["indexDonneePlusRecent"][0]
-  return consoData["details"][lastAvailableData]["valeurs"]["total"]*1000
+# Get Quebec Last 24H Conso in KWh
+def getQuebecLast24HConso(dataPath = r"..\infra\data\2022-sources-electricite-quebec.csv") -> float:
+  elecHistory = pd.read_csv(dataPath)
 
-#Get ratios of each building
-def getRatio()-> pd.Series:
-  return pd.read_json("..\infra\data\consoBatRatio.json")["ratio"]
+  now = (datetime.now(pytz.timezone('America/Toronto'))+ timedelta(hours=-25)).strftime("2022-%m-%d %H:30")
+  startIdx = elecHistory[elecHistory["Date"]==now].index[0]
 
-#Get actual consumption of each building in KWh
-def getBatActualConsumptions()-> float:
-   batConso = getRatio()*getActualConso() 
-   epsilon = np.random.normal(0, 0.1, len(batConso))
+  return elecHistory.iloc[startIdx:startIdx+24]["Total "].sum()*1000
+
+#Get building ratio
+def getRatio()->pd.Series:
+  return pd.read_json(r"..\infra\data\ratio_edifice_electricite.json")["ratio"]
+
+# Get building Last 24H conso in KWh
+def getBatLast24HConso()->list[float]:
+   batConso = getRatio()*getQuebecLast24HConso() 
+   epsilon = np.random.normal(loc = -0.1, scale=0.2, size= len(batConso)) #A jouer dessus pour jouer sur le epsilon ajouter pour les résultats
    return list(batConso + epsilon*batConso)
 
-# Generate 2022's 3 months consumption in KWh
-def getBatLastYear3MonthsConsumptions() -> float:
-  consoQuebecJournalier2022 = pd.read_csv("..\infra\data\2022-sources-electricite-quebec.csv")
-  actualMonth = datetime.now().month
-  if actualMonth > 1 : startMonth = str(actualMonth -1)
-  else : startMonth = str(1)
+# Get building Last year's month conso in KWh
+def getQuebecLastYearMonthConsumptions(dataPath = r"..\infra\data\2021-sources-electricite-quebec.xlsx")->pd.Series:
+  lastYearConso = pd.read_excel(dataPath)
+  nowMonth = datetime.now(pytz.timezone('America/Toronto')).month
+  LastYearMonthsConsumptions = 0
+  for i in range(len(lastYearConso)):
+    if lastYearConso["Date"][i].month == nowMonth: 
+      LastYearMonthsConsumptions += lastYearConso["Total "][i]
+  return LastYearMonthsConsumptions*1000
+# Get building Last year's month conso in KWh
 
-  if len(startMonth) == 1:
-    startMonth = "0"+startMonth
+def getBatLastYearMonthConsumptions(dataPath = r"..\infra\data\2021-sources-electricite-quebec.xlsx")->pd.Series:
+  return getRatio()*getQuebecLastYearMonthConsumptions()
+
+# Get day average of building Last year's month conso in KWh
+def getDayAverageQuebecLastYearMonthConsumptions()->pd.Series:
+  return getQuebecLastYearMonthConsumptions()/31 # Pour la demo on est en Mars donc 31 jours mais à refaire pour que ça soit plus clean
+
+# Get day average of building Last year's month conso in KWh
+def getDayAverageBatLastYearMonthConsumptions()->pd.Series:
+  return getBatLastYearMonthConsumptions()/31 # Pour la demo on est en Mars donc 31 jours mais à refaire pour que ça soit plus clean
   
-  startIdx = consoQuebecJournalier2022[consoQuebecJournalier2022["Date"] == "2022-"+startMonth+"-01 00:30"].index[0]
-  return consoQuebecJournalier2022.iloc[startIdx: min(len(consoQuebecJournalier2022),startIdx+90)]["Total "]*1000
+# Compute numeric score
+def getScoreNumeric()->pd.Series:
+  batsAverageLastYearDay = getDayAverageBatLastYearMonthConsumptions()
+  batLast24HConso = getBatLast24HConso()
+  return (batLast24HConso - batsAverageLastYearDay)/batsAverageLastYearDay
 
-def getHourAverageConsumptionOfLastYear3Months()-> float:
-  return np.mean(getBatLastYear3MonthsConsumptions())
-  
-
-def getScoreNumeric()-> pd.Series:
-  previousAverageBatConsumptionPerHour = getHourAverageConsumptionOfLastYear3Months() * getRatio()
-  return (getBatActualConsumptions() - previousAverageBatConsumptionPerHour)/previousAverageBatConsumptionPerHour
-
-def getScoreLetter()-> list[str]:
+# Compute letter score
+def getScoreLetter()->list[str]:
   numScores = getScoreNumeric()
   letterScores = []
   for i in range(len(numScores)):
-    if numScores[i] <= -0.3: letterScores.append("A")
-    elif numScores[i] <= -0.10: letterScores.append("B")
-    elif numScores[i] <= 0.02: letterScores.append("C")
-    elif numScores[i] <= 0.15: letterScores.append("D")
+    if numScores[i] <= -0.15: letterScores.append("A")
+    elif numScores[i] <= -0.05: letterScores.append("B")
+    elif numScores[i] <= 0: letterScores.append("C")
+    elif numScores[i] <= 0.10: letterScores.append("D")
     else : letterScores.append("E")
+  return letterScores
+  
+# Get JsonFormat for building name position and numeric score
+def getJsonPostionAndScoreNumeric()-> str:
+  batData = pd.read_json(r"..\infra\data\ratio_edifice_electricite.json")
+  batData["score"] = getScoreNumeric()
+  return batData[["Nom","Latitude","Longitude","score"]].to_json(orient="index")
+
+# Get JsonFormat for building name position and letter score
+def getJsonPostionAndScoreLetter()-> str:
+  batData = pd.read_json(r"..\infra\data\ratio_edifice_electricite.json")
+  batData["score"] = getScoreLetter()
+  return batData[["Nom","Latitude","Longitude","score"]].to_json(orient="index")
+
+# Get JsonFormat for top 3 building with name position and numeric score
+def getJsonPodiumScore()->str:
+  batData = pd.read_json(r"..\infra\data\ratio_edifice_electricite.json")
+  batData["score"] = getScoreNumeric()
+  return batData[["Nom","Latitude","Longitude","score"]].sort_values(by=['score']).iloc[:3].reset_index(drop=True).to_json(orient="index")
+
+# Get JsonFormat for top 3 building with name position and numeric score
+def getJsonPodiumConso()->str:
+  batData = pd.read_json(r"..\infra\data\ratio_edifice_electricite.json")
+  batData["score"] = getScoreNumeric()*getDayAverageBatLastYearMonthConsumptions() * (365*3.5/1500000)
+  return batData[["Nom","Latitude","Longitude","score"]].sort_values(by=['score']).iloc[:3].reset_index(drop=True).to_json(orient="index")
