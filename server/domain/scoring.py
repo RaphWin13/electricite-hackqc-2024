e1d8b4f218ba122bd54485d1c7344bce8cbf710c
Félix Dupré-Ouellet <f.dupre.ouellet@hotmail.ca>
2024-03-13 21:36:23 -0400

    Working back-end

--- server/domain/scoring.py
+++ server/domain/scoring.py
@@ -1,59 +1,61 @@
 import pandas as pd
 import numpy as np
-from datetime import datetime
+from datetime import datetime, timedelta
 import pytz
 
-from datetime import datetime
+EDIFICES_FILE = r"infra/data/edifices_GES_electricite.csv"
+RATIO_FILE = r"infra/data/ratio_edifice_electricite.json"
+SOURCES_2022_FILE = r"infra/data/download/2022-sources-electricite-quebec.csv"
+SOURCES_2021_FILE = r"infra/data/download/2021-sources-electricite-quebec.csv"
 
 # Load Data de Laurence
-def load_data(string_path:str, sep:str=";"
-              )->pd.DataFrame:
+def load_csv(string_path:str, sep:str=",", parse_dates=None)->pd.DataFrame:
     """
     Load the data from a relative path
     """
     with open(string_path, "r") as f:
-        content = pd.read_csv(f, sep=sep)
+        content = pd.read_csv(f, sep=sep, parse_dates=parse_dates, )
     return content 
     
 # Create ratio File
 def createBuildingRatioFile():
-  buildingConso = load_data(r"..\infra\data\edifices_GES_electricite.csv") #Laurence file is read hree
-  lastYearDailyQuebecConso = pd.read_excel("..\infra\data\2022-sources-electricite-quebec.xlsx") #Ratio a partir de 2022
+  buildingConso = load_csv(EDIFICES_FILE, sep=';') #Laurence file is read hree
+  lastYearDailyQuebecConso = load_csv("infra/data/download/2022-sources-electricite-quebec.csv", parse_dates=["Date"]) #Ratio a partir de 2022
   totalLastYearKWh = lastYearDailyQuebecConso["Total "].sum()*1000
   buildingConso["ratio"]= buildingConso["Electricite"]/totalLastYearKWh 
-  buildingConso.to_json("..\infra\data\ratio_edifice_electricite.json") #Laurence file concat with a column of the building ratio
+  buildingConso.to_json("infra/data/ratio_edifice_electricite.json") #Laurence file concat with a column of the building ratio
 
 # Get Quebec Last 24H Conso in KWh
-def getQuebecLast24HConso(dataPath = r"..\infra\data\2022-sources-electricite-quebec.csv") -> float:
+def getQuebecLast24HConso(dataPath = SOURCES_2022_FILE) -> float:
   elecHistory = pd.read_csv(dataPath)
 
-  now = (datetime.now(pytz.timezone('America/Toronto'))+ timedelta(hours=-25)).strftime("2022-%m-%d %H:30")
+  now = (datetime.now(pytz.timezone('America/Toronto')) + timedelta(hours=-25)).strftime("2022-%m-%d %H:30")
   startIdx = elecHistory[elecHistory["Date"]==now].index[0]
 
   return elecHistory.iloc[startIdx:startIdx+24]["Total "].sum()*1000
 
 #Get building ratio
 def getRatio()->pd.Series:
-  return pd.read_json(r"..\infra\data\ratio_edifice_electricite.json")["ratio"]
+  return pd.read_json(RATIO_FILE, encoding="utf-8")["ratio"]
 
 # Get building Last 24H conso in KWh
 def getBatLast24HConso()->list[float]:
-   batConso = getRatio()*getQuebecLast24HConso() 
-   epsilon = np.random.normal(loc = -0.1, scale=0.2, size= len(batConso)) #A jouer dessus pour jouer sur le epsilon ajouter pour les résultats
-   return list(batConso + epsilon*batConso)
+  batConso = getRatio()*getQuebecLast24HConso() 
+  epsilon = np.random.normal(loc = -0.1, scale=0.2, size= len(batConso)) #A jouer dessus pour jouer sur le epsilon ajouter pour les résultats
+  return list(batConso + epsilon*batConso)
 
 # Get building Last year's month conso in KWh
-def getQuebecLastYearMonthConsumptions(dataPath = r"..\infra\data\2021-sources-electricite-quebec.xlsx")->pd.Series:
-  lastYearConso = pd.read_excel(dataPath)
+def getQuebecLastYearMonthConsumptions(dataPath = SOURCES_2021_FILE)->pd.Series:
+  lastYearConso = load_csv(dataPath, parse_dates=["Date"])
   nowMonth = datetime.now(pytz.timezone('America/Toronto')).month
   LastYearMonthsConsumptions = 0
   for i in range(len(lastYearConso)):
     if lastYearConso["Date"][i].month == nowMonth: 
       LastYearMonthsConsumptions += lastYearConso["Total "][i]
   return LastYearMonthsConsumptions*1000
-# Get building Last year's month conso in KWh
 
-def getBatLastYearMonthConsumptions(dataPath = r"..\infra\data\2021-sources-electricite-quebec.xlsx")->pd.Series:
+# Get building Last year's month conso in KWh
+def getBatLastYearMonthConsumptions(dataPath = SOURCES_2021_FILE)->pd.Series:
   return getRatio()*getQuebecLastYearMonthConsumptions()
 
 # Get day average of building Last year's month conso in KWh
@@ -84,24 +86,24 @@ def getScoreLetter()->list[str]:
   
 # Get JsonFormat for building name position and numeric score
 def getJsonPostionAndScoreNumeric()-> str:
-  batData = pd.read_json(r"..\infra\data\ratio_edifice_electricite.json")
+  batData = pd.read_json(RATIO_FILE)
   batData["score"] = getScoreNumeric()
   return batData[["Nom","Latitude","Longitude","score"]].to_json(orient="index")
 
 # Get JsonFormat for building name position and letter score
 def getJsonPostionAndScoreLetter()-> str:
-  batData = pd.read_json(r"..\infra\data\ratio_edifice_electricite.json")
+  batData = pd.read_json(RATIO_FILE)
   batData["score"] = getScoreLetter()
-  return batData[["Nom","Latitude","Longitude","score"]].to_json(orient="index")
+  return batData[["Nom","Latitude","Longitude","score"]].to_json(orient="index", force_ascii=False)
 
 # Get JsonFormat for top 3 building with name position and numeric score
 def getJsonPodiumScore()->str:
-  batData = pd.read_json(r"..\infra\data\ratio_edifice_electricite.json")
+  batData = pd.read_json(RATIO_FILE)
   batData["score"] = getScoreNumeric()
-  return batData[["Nom","Latitude","Longitude","score"]].sort_values(by=['score']).iloc[:3].reset_index(drop=True).to_json(orient="index")
+  return batData[["Nom","Latitude","Longitude","score"]].sort_values(by=['score']).iloc[:3].reset_index(drop=True).to_json(orient="index", force_ascii=False)
 
 # Get JsonFormat for top 3 building with name position and numeric score
 def getJsonPodiumConso()->str:
-  batData = pd.read_json(r"..\infra\data\ratio_edifice_electricite.json")
+  batData = pd.read_json(RATIO_FILE)
   batData["score"] = getScoreNumeric()*getDayAverageBatLastYearMonthConsumptions() * (365*3.5/1500000)
   return batData[["Nom","Latitude","Longitude","score"]].sort_values(by=['score']).iloc[:3].reset_index(drop=True).to_json(orient="index")
--- server/main.py
+++ server/main.py
@@ -1,10 +1,12 @@
 from flask import Flask
 
 from api.HealthResource import HealthResource
+from api.ScoreResource import ScoreResource
 
 app = Flask(__name__)
 
 health = HealthResource(app)
+score = ScoreResource(app)
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port=5000)
